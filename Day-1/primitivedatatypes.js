b73025200d1ad2accd7be10bd24e5e3e735c239f
IshratBaig <143535339+IshratBaig@users.noreply.github.com>
2025-12-01 23:02:26 +0530

    Add files via upload
    Week1

---
+++ Day-1/primitivedatatypes.js
@@ -0,0 +1,15 @@
+let firstName='Ishrat'
+let companyName='Amazon'
+let mobileNumber=9898989898
+let isAutomation=true
+let hasplaywright
+console.log(typeof firstName)
+console.log(typeof companyName)
+console.log(typeof mobileNumber)
+console.log(typeof isAutomation)
+console.log(typeof hasplaywright)
+console.log(`firstName is ${firstName}`)
+console.log(`companyName is ${companyName}`)
+console.log(`mobileNumber is ${mobileNumber}`)
+console.log(`isAutomation is ${isAutomation}`)
+console.log(`hasplaywright is ${hasplaywright}`)
